4c1a82f753bab2fdad99540be82e0fcbc2a59e34
Anna Kafka <anna.kafka933@gmail.com>
2025-04-26 22:10:05 +0200

    Remove 'active' class from links and articles when link is clicked

--- js/script.js
+++ js/script.js
@@ -13,7 +13,11 @@ function titleClickHandler(event) {
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll(".posts .post.active");
 
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove("active");
+  }
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
