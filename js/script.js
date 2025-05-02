15fee054b8a4a2b70f2176ec66e4d6a73015393b
Anna Kafka <anna.kafka933@gmail.com>
2025-05-02 14:59:49 +0200

    Add generation of tag list with counters in the right column

--- js/script.js
+++ js/script.js
@@ -270,7 +270,18 @@ function generateTags(){
   /* [NEW] find list of tags in right column */
   const tagList = document.querySelector(optTagsListSelector);
 
-  /* [NEW] add html from allTags to tagList */
-  //tagList.innerHTML = allTags.join(' ');
-  console.log(allTags);
+/* [NEW] create variable for all links HTML code */
+let allTagsHTML = '';
+
+/* [NEW] START LOOP: for each tag in allTags: */
+for (let tag in allTags) {
+
+  /* [NEW] generate code of a link and add it to allTagsHTML */
+  allTagsHTML += '<li><a href="#tag-' + tag + '">' + tag + '</a> (' + allTags[tag] + ')</li>';
+}
+
+/* [NEW] END LOOP: for each tag in allTags: */
+
+/* [NEW] add html from allTagsHTML to tagList */
+tagList.innerHTML = allTagsHTML;
 }
